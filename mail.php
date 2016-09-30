c1d9ff786cb1170bff385c534e68ce4843eb3276
Brett D Burdick <burdickbrett@outlook.com>
2016-09-29 22:06:53 -0400

    testing mailer

--- mail.php
+++ mail.php
@@ -21,23 +21,23 @@
     $to = 'burdickbrett@gmail.com'; 
     $subject = 'Email Inquiry from Web Portfolio';
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-    if(isset($_POST['g-recaptcha-response'])){
-          $captcha=$_POST['g-recaptcha-response'];
-    }
-    if(!$captcha){
-          echo '<h2>Please check the the captcha form.</h2>';
-          exit;
-    }
-    $secretKey = "6LfT7gcUAAAAAHIemAOFDIyeKcqppQ7G-y8Acn3p";
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
-    $responseKeys = json_decode($response,true);
-    if(intval($responseKeys["success"]) !== 1) {
-        echo '<h2>You are spammer ! Get the @$%K out</h2>';
-    } 
-    else {
-        echo '<h2>Your message was sent. Thank you!</h2>';
-    }
+    // if(isset($_POST['g-recaptcha-response'])){
+    //       $captcha=$_POST['g-recaptcha-response'];
+    // }
+    // if(!$captcha){
+    //       echo '<h2>Please check the the captcha form.</h2>';
+    //       exit;
+    // }
+    // $secretKey = "6LfT7gcUAAAAAHIemAOFDIyeKcqppQ7G-y8Acn3p";
+    // $ip = $_SERVER['REMOTE_ADDR'];
+    // $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+    // $responseKeys = json_decode($response,true);
+    // if(intval($responseKeys["success"]) !== 1) {
+    //     echo '<h2>You are spammer ! Get the @$%K out</h2>';
+    // } 
+    // else {
+    //     echo '<h2>Your message was sent. Thank you!</h2>';
+    // }
 
 
 
@@ -71,13 +71,13 @@
     // $to = 'burdickbrett@gmail.com'; 
     // $subject = 'Email Inquiry from Web Portfolio';
     // $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-    //   if ($_POST['submit']) {
-    //       if (mail ($to, $subject, $body, $from)) { 
-    //           echo '<p>Thank you for your email!</p>';
-    //       } else { 
-    //           echo '<p>Oops! An error occurred. Try sending your message again.</p>'; 
-    //       }
-    //   }
+      if ($_POST['submit']) {
+          if (mail ($to, $subject, $body, $from)) { 
+              echo '<p>Thank you for your email!</p>';
+          } else { 
+              echo '<p>Oops! An error occurred. Try sending your message again.</p>'; 
+          }
+      }
 
 
 
