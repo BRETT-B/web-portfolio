140f41b9dd33a20d33b2ca5376311151a633dee3
Brett D Burdick <burdickbrett@outlook.com>
2016-10-01 23:48:35 -0400

    fixed jQuery syntax errors

--- js/submit-form.js
+++ js/submit-form.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
-	$('form.contact').on('submit', function(){
+
+	$('form.contact').on('submit', function(event){
+		event.preventDefault();
 		var yourname = $('#yourname').val();
 		var email = $('#email').val();
 		var message = $('#message').val();
 
-		if (yourname == '' || email == '' || message = ''){
+		if (yourname == '' || email == '' || message ==''){
 			$('#error-message').html('All fields are required');
 		}
 		else {
 			$('#error-message').html('');
 			$.ajax({
-				url: 'mail.php',
+				url: '/mail.php',
 				type: 'POST',
 				data: {yourname:yourname, email:email, message:message},
 				success: function(response){
 					$('input[type=text], input[type=email], textarea').val('');
 					$('#success-message').fadeIn().html(response);
-					setTimeout(function({
+					setTimeout(function(){
 						$('#success-message').fadeOut('slow');
 					}, 2000);
 
