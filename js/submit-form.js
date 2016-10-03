61e0475d2fa6e7ada07a8d7735a9370398857a84
Brett D Burdick <burdickbrett@outlook.com>
2016-10-03 17:20:51 -0400

    test contact

--- js/submit-form.js
+++ js/submit-form.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
 		var yourname = $('#yourname').val();
 		var email = $('#email').val();
 		var message = $('#message').val();
-		var formData = {yourname: yourname, email: email, message: message}
 
 		if (yourname == '' || email == '' || message ==''){
 			$('#error-message').html('All fields are required');
@@ -15,14 +14,22 @@ $(document).ready(function() {
 			$.ajax({
 				type: 'POST',
 				url: 'mail.php',
-				data: formData,
+				data: {
+					yourname: yourname,
+					email: email,
+					message: message
+				},
 				success: function(response){
+					console.log('Error:'response)
 					$('input[type=text], input[type=email], textarea').val('');
 					$('#success-message').fadeIn().html(response);
 					setTimeout(function(){
 						$('#success-message').fadeOut('slow');
 					}, 2000);
 
+				}, error: function(error){
+					console.log(error);
+					alert("Post error");
 				}
 			});
 		}
